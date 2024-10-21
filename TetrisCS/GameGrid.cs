715180cb3917ba70412b3b1f5b540b7bffd417c2
Lucas Lobell <lucaslobell2@gmail.com>
2024-10-21 10:24:20 -0300

    first commit

---
+++ TetrisCS/GameGrid.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TetrisCS
+{
+    internal class GameGrid
+    {
+    }
+}
