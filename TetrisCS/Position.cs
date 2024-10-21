9741895691e6d55557dc1557e535846358be585c
Lucas Lobell <lucaslobell2@gmail.com>
2024-10-21 11:34:01 -0300

    blocks

--- TetrisCS/Position.cs
+++ TetrisCS/Position.cs
@@ -1,12 +1,14 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace TetrisCS
+﻿namespace TetrisCS
 {
-    internal class Position
+    public class Position
     {
+        public int Row { get; set; }
+        public int Column { get; set; }
+
+        public Position(int row, int column)
+        {
+            Row = row;
+            Column = column;
+        }
     }
 }
